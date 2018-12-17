6cee76280697d88bfdb91dadc06d54ec003cd3b8
Rosemberg Torres <rosemberg@opovodigital.com>
2018-12-17 17:39:29 -0300

    Recuperando dados do firebase realtime

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const RootStack = createStackNavigator(
     Resultado: ResultadoScreen
   },
   {
-    initialRouteName: "Login"
+    initialRouteName: "Home"
   }
 );
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList
+} from 'react-native';
+import firebase from 'firebase';
+
 
 export default class HomeScreen extends React.Component {
+
+
+  componentDidMount() {
+    const bd = firebase.database().ref();
+
+    for (i = 1; i < 6; i++) { 
+      const bdPerguntas = bd.child('Perguntas/' + i);
+      bdPerguntas.on("value", function (snapshot) {
+        snapshot.forEach((pergunta) => {
+          let NumeroDaPergunta = pergunta.key
+          console.log("New high score: " + NumeroDaPergunta);
+        });
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+    }        
+  }
+
+  constructor(props) {
+    super(props);
+    this.setState({
+      quiz: []
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home</Text>
+
       </View>
     );
   }
